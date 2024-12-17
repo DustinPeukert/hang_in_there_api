fbc2db69242cd8835a70f73b49e45a0ca64d7fd9
Dustin Peukert <dpeukert04@gmail.com>
2024-12-17 17:20:46 -0500

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,30 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Poster.destroy_all
+
+Poster.create(name: "REGRET",
+              description: "Hard work rarely pays off.",
+              price: 89.00,
+              year: 2019,
+              vintage: true,
+              img_url: "https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d"
+)
+
+Poster.create(name: "DISAPPOINTMENT",
+              description: "This is what your parents think of you.",
+              price: 108.00,
+              year: 2020,
+              vintage: false,
+              img_url: "https://images.unsplash.com/photo-1620401537439-98e94c004b0d"
+)
+
+Poster.create(name: "SADNESS",
+              description: "Let the depression take over.",
+              price: 65.00,
+              year: 2018,
+              vintage: false,
+              img_url: "https://images.unsplash.com/photo-1551993005-75c4131b6bd8"
+)
+
+puts "Seeded Successfully!"
